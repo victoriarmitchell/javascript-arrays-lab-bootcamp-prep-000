f641cf645055272ec744ba16f25fcff854c69d83
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-06 00:12:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var kittens = ['Milo', 'Otis', 'Garfield']; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  unshit()
+  kittens.unshift(name)
+  return kittens
 }
